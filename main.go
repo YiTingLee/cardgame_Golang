193b1283bccbecca2dca530c4519975e409fa688
YiTingLee <ranye0719@gmail.com>
2017-09-26 23:21:10 +0800

    able to read cards data from file, and it has error handling

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards")
 
-	// hand, remainingCards := deal(cards, 2)
-
-	// hand.print()
-	// remainingCards.print()
-
-	cards := newDeck()
-	fmt.Println(cards.toString())
-	cards.saveToFile("my_cards")
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
