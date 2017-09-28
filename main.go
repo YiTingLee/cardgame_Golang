040ea7f991d51931acb1cd0202111f97bfb812fa
yitinglee <ranye0719@gmail.com>
2017-09-28 14:15:53 +0800

    shuffle is added

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 func main() {
-	// cards := newDeck()
-	// fmt.Println(cards.toString())
-	// cards.saveToFile("my_cards")
-
-	cards := newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
