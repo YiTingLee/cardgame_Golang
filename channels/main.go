c8cd41dd85fbf02cb7e82ad0c4c3e36bdccd6a98
YiTingLee <ranye0719@gmail.com>
2017-10-01 00:47:08 +0800

    Repeating Routines

--- channels/main.go
+++ channels/main.go
@@ -18,17 +18,29 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	fmt.Println(<-c)
+	// }
+
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep it's up"
+	c <- link
 }
